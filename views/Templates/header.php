0b847f9be230089ff3179ede5d6bf470db9d25ef
EdwinSiapno <siapno1701@gmail.com>
2017-04-03 12:46:29 +0800

    Add files via upload

---
+++ views/Templates/header.php
@@ -0,0 +1,12 @@
+<html>
+        <head>
+                <title>CodeIgniter Tutorial</title>
+
+                <link rel="stylesheet" href="https://bootswatch.com/darkly/bootstrap.min.css">
+
+
+
+        </head>
+        <body>
+
+                <h1><?php echo $title; ?></h1>
